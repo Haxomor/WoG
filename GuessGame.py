c26479b1874ec0ae361aa4773e63d882ffa26b9f
Haxomor <96199749+Haxomor@users.noreply.github.com>
2024-04-13 15:17:05 +0300

    Update GuessGame.py

--- GuessGame.py
+++ GuessGame.py
@@ -1,42 +1,22 @@
-# import random
-#
-# secret_num = random.randint(1, 100)
-# tries = 3
-#
-# class guess_class:
-#     def guess(diff):
-#         global tries
-#         while tries > 0:
-#
-#             difficulty = input(f"Guess the secret number: ")
-#             if int(difficulty) != secret_num:
-#                 print(f"{difficulty} isn't the number.")
-#                 tries -= 1
-#                 print(f"You have {tries} tries left.")
-#             else:
-#                 print("Congratulations! You guessed the correct number.")
-#                 break
-#         else:
-#             print(f"Sorry, you've run out of tries. The correct number was {secret_num}.")
 
 import random
 
 def guess(diff):
     secret_num = random.randint(1, 25)
     tries = 3
+    print(secret_num)
     while tries > 0:
-        difficulty = input("Guess the secret number 1-25: ")
-        if not difficulty.isdigit():
+        guess_num = input("Guess the secret number 1-25: ")
+        if not guess_num.isdigit():
             print("Please enter a valid number.")
             continue
 
-        difficulty = int(difficulty)
-        if difficulty != secret_num:
-            print(f"{difficulty} isn't the number.")
+        guess_num = int(guess_num)
+        if guess_num != secret_num:
+            print(f"{guess_num} isn't the number.")
             tries -= 1
             print(f"You have {tries} tries left.")
         else:
-            print("Congratulations! You guessed the correct number.")
-            break
-    else:
-        print(f"Sorry, you've run out of tries. The correct number was {secret_num}.")
+            return True  # Return True when the correct number is guessed
+    print(f"Sorry, you've run out of tries. The correct number was {secret_num}.")
+    return False  # Return False if the user runs out of tries
